2cd8a9073c473945b2f75de35e00e1ff6af0d219
BFSMx1 <90730982+alberto-iram@users.noreply.github.com>
2022-10-06 13:40:50 -0500

    Update addButtons.js

--- js/addButtons.js
+++ js/addButtons.js
@@ -2,13 +2,13 @@ AFRAME.registerComponent("create-buttons", {
   init: function() {
     // 1. Crea el botón
     var button1 = document.createElement("button");
-    button1.innerHTML = "RATE DISH";
+    button1.innerHTML = "CALIFICAR";
     button1.setAttribute("id", "rating-button");
     button1.setAttribute("class", "btn btn-warning mr-3");
 
     // 2. Crea el botón
     var button2 = document.createElement("button");
-    button2.innerHTML = "ORDER NOW";
+    button2.innerHTML = "ORDENAR";
     button2.setAttribute("id", "order-button");
     button2.setAttribute("class", "btn btn-warning mr-4");
 
