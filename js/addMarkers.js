fee1170b4dc7c15d7df2861d1bab71d23e0f1aad
BFSMx1 <90730982+alberto-iram@users.noreply.github.com>
2022-10-06 17:20:20 -0500

    Update addMarkers.js

--- js/addMarkers.js
+++ js/addMarkers.js
@@ -110,7 +110,7 @@ AFRAME.registerComponent("create-markers", {
           color: "white",
           width: 3,
           align: "center",
-          value: `Por solo\n $${dish.price}`
+          value: `Solo\n $${dish.price}`
         });
 
       
